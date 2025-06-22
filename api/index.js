91e4e227acb6a2028bd2eda00f053a60d7be56a9
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2025-06-22 13:23:09 -0300

    s

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const jsonServer = require("json-server");
+const cors = require("cors"); // <--- importe isso
 const server = jsonServer.create();
-const path = require("path");
-const router = jsonServer.router(path.join(__dirname, "..", "db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); // <--- adicione isso
 server.use(middlewares);
 server.use(router);
 
 module.exports = (req, res) => {
   server(req, res);
-};
+};  
